b386dfd43278673c80d440bdc9964188c010fb8d
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-16 14:37:51 +0300

    add the insta_app to the installed app

--- instagram/settings.py
+++ instagram/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'insta_app.apps.InstaAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'insta_app.apps.InstaAppConfig',
+
 ]
 
 MIDDLEWARE = [
