e69526a0efdf15d73866b6e16493b65a5eba8866
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-16 19:24:55 +0300

    create the user and comment models

--- insta_app/models.py
+++ insta_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    image_path = models.ImageField(upload_to='images/', blank=False)
+    insta_story = models.TextField()
+    likes = models.IntegerField()
+
+    def __str__(self):
+        return self.username
+
+
+class Comment(models.Model):
+    user_comment = models.TextField()
