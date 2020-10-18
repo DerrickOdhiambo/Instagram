5f195147175f122a0ec4086bf1cb819628cb3a26
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-18 19:42:33 +0300

    create the Image model and function methods

--- insta_app/models.py
+++ insta_app/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    image_path = models.ImageField(upload_to='images/', blank=False)
-    insta_story = models.TextField()
+class Image(models.Model):
+    image = models.ImageField(upload_to='posts/', blank=False)
+    image_name = models.CharField(max_length=30)
+    image_caption = models.TextField()
+    post_creator = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.IntegerField()
+    comments = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return self.image_name
 
+    def save_image(self):
+        self.save()
 
+    def delete_image(self):
+        pass
+
+    def update_caption(self):
+        pass
