8c0c6e654edbceb658bae0f4dad2b4b09ac2e001
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-19 08:45:42 +0300

    install a package for resizing the profile images

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
 
 class Profile(models.Model):
-    profile_picture = models.ImageField(
-        default='default.jpg', upload_to='profile_pics/')
+    profile_picture = ResizedImageField(size=[300, 300], quality=75,
+                                        default='default.jpg', upload_to='profile_pics/')
     user_bio = models.TextField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
