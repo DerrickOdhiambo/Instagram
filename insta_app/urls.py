2706358b6ac30593b0794e2f967b26cd4a651800
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-20 00:25:33 +0300

    create a route for the like button

--- insta_app/urls.py
+++ insta_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ImageListView
+from .views import ImageListView, ImageDetailView, ImageCreateView, LikeView, ImageUpdateView
 from . import views
 
 
 urlpatterns = [
     path('', ImageListView.as_view(), name='homepage'),
+    path('like/<int:pk>', LikeView, name='like_image'),
 ]
