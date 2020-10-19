e795e0082b9e99781ec1e50b3495ce3e325510c6
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-19 15:03:37 +0300

    create a clase based view

--- insta_app/views.py
+++ insta_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Image
 
 
 def index(request):
     images = Image.objects.all()
     return render(request, 'index.html', {'images': images})
+
+
+class ImageListView(ListView):
+    model = Image
+    template_name = 'index.html'
+    context_object_name = 'images'
