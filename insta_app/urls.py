9b232babd0dec3b6d3d6350961efad086531d152
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-20 20:14:02 +0300

    create a path for the like view

--- insta_app/urls.py
+++ insta_app/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import ImageListView, ImageDetailView, ImageCreateView, LikeView, ImageUpdateView
+from .views import ImageListView, LikeView, ImageUpdateView, ImageDetailView, ImageCreateView, ImageDeleteView, CommentCreateView
 from . import views
 
 
 urlpatterns = [
     path('', ImageListView.as_view(), name='homepage'),
-    path('like/<int:pk>', LikeView, name='like_image'),
+    path('post/<int:pk>/', ImageDetailView.as_view(), name='image-detail'),
+    path('post/new/', ImageCreateView.as_view(), name='image-create'),
+    path('post/<int:pk>/update/', ImageUpdateView.as_view(), name='image-update'),
+    path('post/<int:pk>/delete/', ImageDeleteView.as_view(), name='image-delete'),
+    path('post/<int:pk>/comment/',
+         CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/like/', LikeView, name='like_image'),
 ]
