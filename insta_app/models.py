1cc4b9f81fa84ac6f24c714c0b61abe2d873ae6a
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-20 20:17:19 +0300

    create a model for the comments and likes

--- insta_app/models.py
+++ insta_app/models.py
@@ -8,8 +8,7 @@ class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_caption = models.TextField()
     post_creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='image_posts')
-    comments = models.TextField()
+    likes = models.IntegerField(default=0)
     date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -25,4 +24,29 @@ class Image(models.Model):
         pass
 
     def get_absolute_url(self):
-        return reverse('image-detail', kwargs={'pk': self.pk})
+        return reverse('image--detail', kwargs={'pk': self.pk})
+
+    def delete_image(self):
+        pass
+
+    def update_caption(self):
+        pass
+
+
+class Comment(models.Model):
+    image = models.ForeignKey(
+        'insta_app.Image', related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return reverse('homepage')
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_likes')
+    image_post = models.ForeignKey(
+        Image, on_delete=models.CASCADE, related_name='image_post_likes')
