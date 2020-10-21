05eb26d5f39617f5a703065a8b675e3309c39da8
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-21 06:41:12 +0300

    changes

---
+++ insta_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    text = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'input is-medium'}), required=False)
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
